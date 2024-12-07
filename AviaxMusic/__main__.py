04a0286cee3943ed1b85c213de3b028e2c6ff2a8
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-07 10:04:20 +0530

    Add files via upload

---
+++ AviaxMusic/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from AviaxMusic import LOGGER, app, userbot
+from AviaxMusic.core.call import Aviax
+from AviaxMusic.misc import sudo
+from AviaxMusic.plugins import ALL_MODULES
+from AviaxMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("AviaxMusic.plugins" + all_module)
+    LOGGER("AviaxMusic.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await Aviax.start()
+    try:
+        await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("AviaxMusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Aviax.decorators()
+    LOGGER("AviaxMusic").info(
+        "Music Star Done"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("AviaxMusic").info("Stopping Music Bot...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
