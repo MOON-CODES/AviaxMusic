d245c2146e2e8efd4de5055573df7ba595991f1b
Moon <101085046+MOON-CODES@users.noreply.github.com>
2025-01-04 22:06:55 +0530

    Update playmode.py

--- AviaxMusic/cplugin/playmode.py
+++ AviaxMusic/cplugin/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from SHUKLAMUSIC import app
-from SHUKLAMUSIC.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from SHUKLAMUSIC.utils.decorators import language
-from SHUKLAMUSIC.utils.inline.settings import playmode_users_markup
+from AviaxMusic import app
+from AviaxMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from AviaxMusic.utils.decorators import language
+from AviaxMusic.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
