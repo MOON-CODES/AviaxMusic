76af8b3593e1c34c1d96735e738175f83b02c990
Moon <101085046+MOON-CODES@users.noreply.github.com>
2025-01-04 22:39:28 +0530

    Update stop.py

--- AviaxMusic/cplugin/stop.py
+++ AviaxMusic/cplugin/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from SHUKLAMUSIC import app
-from SHUKLAMUSIC.core.call import SHUKLA
-from SHUKLAMUSIC.utils.database import set_loop
-from SHUKLAMUSIC.utils.decorators import AdminRightsCheck
-from SHUKLAMUSIC.utils.inline import close_markup
+from AviaxMusic import app
+from AviaxMusic.core.call import Aviax
+from AviaxMusic.utils.database import set_loop
+from AviaxMusic.utils.decorators import AdminRightsCheck
+from AviaxMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -21,7 +21,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await SHUKLA.stop_stream(chat_id)
+    await Aviax.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
