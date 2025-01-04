44f150319ff531d202af592ef7255d7a42a37f60
Moon <101085046+MOON-CODES@users.noreply.github.com>
2025-01-04 23:10:59 +0530

    Update Shiv.py

--- Shiv.py
+++ Shiv.py
@@ -13,7 +13,7 @@ def create_app():
     class Greeting(Resource):
         def get(self):
             logger.info("ɢʀᴇᴇᴛɪɴɢ ᴇɴᴅᴘᴏɪɴᴛ ᴡᴀꜱ ʀᴇᴀᴄʜᴇᴅ")
-            return "ꜱʜᴜᴋʟᴀ ɪꜱ ᴜᴘ ᴀɴᴅ ʀᴇᴀᴅʏ ꜰᴏʀ ᴅᴇꜱᴛʀᴜᴄᴛɪᴏɴ!"
+            return "Aviax ɪꜱ ᴜᴘ ᴀɴᴅ ʀᴇᴀᴅʏ ꜰᴏʀ ᴅᴇꜱᴛʀᴜᴄᴛɪᴏɴ!"
 
     api.add_resource(Greeting, '/')
     
